fae83c54baf980ad256776e196aa9d99e5854dca
Braulio95 <braulioestradaalfonso@hotmail.com>
2023-12-13 11:13:14 -0600

    first commit of the project

---
+++ src/components/ArticlePreview.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>Hola mundo mundial!!!!!!!!</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ArticlePreview",
+});
+
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark fixed-top bg-light d-flex justify-content-center">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Braulio's blog</a>
+        </div>
+    </nav>  
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NavBar",
+});
+</script>
+
+<style scoped>
+.container-fluid{
+    max-width: 1280px;
+}
+.navbar{
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+</style>
